effa8893f1576402d3c79d15d686de05d5ee5eda
Cristofor Rotsching <cristoforrotsching@MacBook-Pro-Cristofor.local>
2026-04-28 14:06:41 +0300

    PAData functions implemented

--- Documents/PA-Library/PAData.cs
+++ Documents/PA-Library/PAData.cs
@@ -4,6 +4,15 @@ using System.Transactions;
 
 struct PAData
 {
+    [DllImport("palibrary.dll")]
+        public static extern void PADataConstruct();
+        [DllImport("palibrary.dll")]
+        public static extern void PADataRuin();
+        [DllImport("palibrary.dll")]
+        public static extern void PADataInit();
+        [DllImport("palibrary.dll")]
+        public static extern void PADataDelete();
+    //struct PAResource
     public int value;//todo use in unsafe contexts
 
     public static implicit operator byte(PAData d) => (byte)d.value;
