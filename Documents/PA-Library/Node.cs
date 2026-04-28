e2c67e0b0bf278a7a3ccbc8303c979d9d56b80a7
Cristofor Rotsching <cristoforrotsching@MacBook-Pro-Cristofor.local>
2026-04-28 14:43:49 +0300

    PANod functions implemented

--- Documents/PA-Library/Node.cs
+++ Documents/PA-Library/Node.cs
@@ -1,9 +1,9 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-//namespace PA_Library
-//{
+namespace PA_Library
+{
 //    internal class Node<T> : Comparer<T>
 //    {
 //        protected internal T Value;
