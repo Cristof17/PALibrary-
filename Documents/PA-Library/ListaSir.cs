b1175cbe96703b107f6b138d303b4148f73a2bcc
Cristofor Rotsching <cristoforrotsching@MacBook-Pro-Cristofor.local>
2026-05-08 11:54:50 +0300

    updated ListaSir.cs with Construct Dlete Init Ruin

--- Documents/PA-Library/ListaSir.cs
+++ Documents/PA-Library/ListaSir.cs
@@ -3,6 +3,16 @@ namespace PA_Library
 {
     public class ListaSir
     {
-    
+        [DllImport("pa-library")]
+        public static extern ArrayListConstruct();
+        [DllImport("pa-library")]
+        public static extern ArrayListRuin();
+        [DllImport("pa-library")]
+        public static extern ArrayListInit();
+        [DllImport("pa-library")]
+        public static extern ArrayListDelete();
+        [StructLayout(LayoutKind.Sequential)]
+        private struct ArrayList List;
+        // int[] objects;
     }
 }
