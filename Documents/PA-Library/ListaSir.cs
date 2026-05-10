e09329f5e212ecfece63d08ce3c7f28d5d83e298
Cristofor Rotsching <cristoforrotsching@MacBook-Pro-Cristofor.local>
2026-05-10 10:00:28 +0300

    ArrayList.cs

--- Documents/PA-Library/ListaSir.cs
+++ Documents/PA-Library/ListaSir.cs
@@ -4,13 +4,13 @@ namespace PA_Library
     public class ListaSir
     {
         [DllImport("pa-library")]
-        public static extern ArrayListConstruct();
+        public static extern struct ArrayList ArrayListConstruct(struct ArrayListPosition place, int[] objects);
         [DllImport("pa-library")]
-        public static extern ArrayListRuin();
+        public static extern void ArrayListRuin(struct ArrayListPosition place, int[] objects);
         [DllImport("pa-library")]
-        public static extern ArrayListInit();
+        public static extern struct ArrayList ArrayListInit(struct ArrayList List);
         [DllImport("pa-library")]
-        public static extern ArrayListDelete();
+        public static extern void ArrayListDelete();
         [StructLayout(LayoutKind.Sequential)]
         private struct ArrayList List;
         // int[] objects;
