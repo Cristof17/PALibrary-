452570dbee56c86005fec49d9deabebd2724cb51
Cristofor Rotsching <cristoforrotsching@MacBook-Pro-Cristofor.local>
2026-05-10 10:05:21 +0300

    PAPair.cs

--- Documents/PA-Library/Pereche.cs
+++ Documents/PA-Library/Pereche.cs
@@ -5,13 +5,13 @@ namespace PA_Library
     struct Pereche
     {
         [DllImport("pa-library")]
-        public static extern PAPAirInit();
+        public static extern struct PAElement PAPairInit(struct PAElement e);
         [DllImport("pa-library")]
-        public static extern PAPairRuin();
+        public static extern int PAPairRuin(struct PAData Data, struct PAElement Next, struct PAStatus status);
         [DllImport("pa-library")]
-        public static extern PAPairConstruct();
+        public static extern struct PAPair PAPairConstruct(struct PAElement Node, struct PAElement Neigh);
         [DllImport("pa-library")]
-        public static extern PAPairDelete();
+        public static extern int PAPairDelete(struct PAElement PA);
 
         [StructLayout(LayoutKind.Sequential)]
         private struct PAPair Pair;
