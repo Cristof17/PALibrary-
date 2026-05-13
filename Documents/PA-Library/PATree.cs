bbd7572ed484aa40456bf29088ccf260f1fc5805
Cristofor Rotsching <cristoforrotsching@MacBook-Pro-Cristofor.local>
2026-05-13 16:24:13 +0300

    solved errors

--- Documents/PA-Library/PATree.cs
+++ Documents/PA-Library/PATree.cs
@@ -20,7 +20,7 @@ namespace PA_Library {
         public static extern void PATreeRuin( PACount n, PACount m, PAList adj, PAElement source);
 		[DllImport("palibrary")]
         public static extern PATree PATreeInit(PATree graf);
-		// [DllImport("palibrary")]
+		[DllImport("palibrary")]
         public static extern int PATreeDelete(PATree PA);
 
 		// [StructLayout(LayoutKind.Sequential)]
