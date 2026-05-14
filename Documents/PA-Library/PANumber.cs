d70b0a67dc4bd309911753827feebdaaba6c2964
Cristofor Rotsching <cristoforrotsching@MacBook-Pro-Cristofor.local>
2026-05-14 11:13:32 +0300

    updated PANumber.cs

--- Documents/PA-Library/PANumber.cs
+++ Documents/PA-Library/PANumber.cs
@@ -5,6 +5,8 @@ using System.Runtime.InteropServices;
 [StructLayout(LayoutKind.Explicit)]
 public struct PANumber
 {
+    [FieldOffset(0)]
+    private int val;
     // [StructLayout(LayoutKind.Sequential)]
     // public struct PANumber Number;
 }
