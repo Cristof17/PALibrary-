2c74dac3f718c9b4b774f7da05aa3192b75a4a35
Cristofor Rotsching <cristoforrotsching@MacBook-Pro-Cristofor.local>
2026-05-14 17:14:19 +0300

    Removed linker-script.so

--- Documents/PA-Library/obj/Debug/netstandard2.0/PA-Library.AssemblyInfo.cs
+++ Documents/PA-Library/obj/Debug/netstandard2.0/PA-Library.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PA-Library")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+307b8fa531437da0f27812ed27ea23fdc595dfef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+980e2131e620c5fc7258436b06907f3861a6b2f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PA-Library")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PA-Library")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
