cd3289a391080ce489f0edea872b95a0009aae79
Cristofor Rotsching <cristoforrotsching@MacBook-Pro-Cristofor.local>
2026-05-20 12:35:25 +0300

    PAStatus

--- Documents/PA-Library/PAStatus.cs
+++ Documents/PA-Library/PAStatus.cs
@@ -8,7 +8,15 @@ public struct PAStatus
 {
     // [StructLayout(LayoutKind.Sequential)]
     // struct PAStatus status;
-
+    static extern PAStatus PAStatusPerformInit( PAStatus Status, PAResource Resource);
+    [DllImport("pa")]
+static extern PAStatus PAStatusPerformDelete( PAStatus Status);
+[DllImport("pa")]
+static extern PAStatus PAStatusPerformConstruct();
+[DllImport("pa")]
+static extern PAStatus PAStatusPerformRuin( PAStatus Status);
+[DllImport("pa")]
+static extern  PAStatus PAStatusPerformCopy( PAStatus Status,  PAStatus Status2);
     // [FieldOffset(0)]
     private int Visited;
     public PAStatus(int Visited) : this()
