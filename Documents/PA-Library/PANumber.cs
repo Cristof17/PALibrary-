970c7af471d8647e7608752e78268e9bde3a2436
Cristofor Rotsching <cristoforrotsching@MacBook-Pro-Cristofor.local>
2026-05-20 11:56:24 +0300

    PAList.cs

--- Documents/PA-Library/PANumber.cs
+++ Documents/PA-Library/PANumber.cs
@@ -5,6 +5,16 @@ using System.Runtime.InteropServices;
 [StructLayout(LayoutKind.Explicit)]
 public struct PANumber
 {
+    [DllImport("palibrary")]
+ static extern PANumber PANumberPerformConstruct();
+[DllImport("palibrary")]
+ static extern PANumber PANumberPerformInit(PANumber Number,  char Value);
+[DllImport("palibrary")]
+ static extern PANumber PANumberPerformDelete( PANumber Number);
+[DllImport("palibrary")]
+ static extern PANumber PANumberPerformRuin( PANumber Number);
+[DllImport("palibrary")]
+ static extern PANumber PANumberPerformCopy( PANumber from,  PANumber to);
     // [StructLayout(LayoutKind.Sequential)]
     // public struct PANumber Number;
 }
