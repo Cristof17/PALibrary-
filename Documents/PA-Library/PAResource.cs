bb5be8b6297035b63c971062bb62af933fe10931
Cristofor Rotsching <cristoforrotsching@Cristofors-MacBook-Pro.local>
2026-06-14 12:55:54 +0300

    PAResource function signatures implemented

--- Documents/PA-Library/PAResource.cs
+++ Documents/PA-Library/PAResource.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
         [DllImport("pa")]
         static extern PAResource PAResourcePerformConstruct();
         [DllImport("pa")]
-        static extern PAResource PAResourcePerformInit(PANumber number);
+        static extern PAResource PAResourcePerformInit(PAResource resource, PANumber number);
         [DllImport("pa")]
         static extern int PAResourcePerformFinish(PAResource PA);
         // [DllImport("pa")]
