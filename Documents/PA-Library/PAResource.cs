62e51178b1b9da21a19826011c013b47301e4312
Cristofor Rotsching <cristoforrotsching@Cristofors-MacBook-Pro.local>
2026-06-14 12:50:41 +0300

    PAResource functions signatures imol

--- Documents/PA-Library/PAResource.cs
+++ Documents/PA-Library/PAResource.cs
@@ -1,17 +1,27 @@
 using System;
 using PA_Library;
 using System.Runtime.InteropServices;
-[StructLayout(LayoutKind.Sequential)]
-struct PAResource
+    [StructLayout(LayoutKind.Sequential)]
     // [StructLayout(LayoutKind.Sequential)]
-{
+    struct PAResource
+    {
+        [DllImport("pa")]
+        static extern PAResourcePerformConstruct();
+        [DllImport("pa")]
+        static extern PAResourcePerformInit();
+        [DllImport("pa")]
+        static extern PAResourcePerformFinish();
+        [DllImport("pa")]
+        // static extern PAResourcePerformD();
+        [DllImport("pa")]
+        static extern PAResourcePerformRuin();
     // public PAResource Resource;
 
     // [FieldOffset(0)]
-    public PANumber Value;
-    // public Resursa(int value)
-    public PAResource(int value)
-    {
+        public PANumber Value;
+        // public Resursa(int value)
+        public PAResource(int value)
+        {
 
+        }
     }
-}
