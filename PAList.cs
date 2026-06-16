507c33c5f6426d398c8b8e5428751f2f514e03f4
Cristofor Rotsching <cristoforrotsching@Cristofors-MacBook-Pro.local>
2026-06-16 11:05:42 +0300

    PAList is public

--- PAList.cs
+++ PAList.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 namespace PA_Library
 {
     [StructLayout(LayoutKind.Sequential)]
-    protected struct PAList
+    public struct PAList
     {
         ArrayList edges;
 
