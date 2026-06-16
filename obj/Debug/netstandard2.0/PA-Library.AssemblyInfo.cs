44998681d00a0d63b1aedc774c2db13f4cbf2259
Cristofor Rotsching <cristoforrotsching@Cristofors-MacBook-Pro.local>
2026-06-16 09:19:19 +0300

    Added PATransposeTree and PANormalTree

--- obj/Debug/netstandard2.0/PA-Library.AssemblyInfo.cs
+++ obj/Debug/netstandard2.0/PA-Library.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PA-Library")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb869ae4d88d1900936b4870aa69c9c032d7fc2c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+592f354b3c39a83ab939cbc1481e7ba9137e6d96")]
 [assembly: System.Reflection.AssemblyProductAttribute("PA-Library")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PA-Library")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
