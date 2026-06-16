4b7fbb3d39993e0caf9c2bf5ff2a611caa2865b0
Cristofor Rotsching <cristoforrotsching@Cristofors-MacBook-Pro.local>
2026-06-16 15:52:11 +0300

    updated PASeriesPerformConstruct

--- PASeries.cs
+++ PASeries.cs
@@ -10,6 +10,15 @@ namespace PA_Library
         {
             m = PACount.PACountPerformConstruct();
             m = PACount.PACountPerformCopy(M,m);
+
+            PACount x = PACount.PACountPerformConstruct();
+            while (x < M)
+            {
+                PAList aux = PAList.PAListPerformConstruct();
+                aux = PAList.PAListPerformCopy(ADJ[x],aux);
+                adj[x] = PAList.PAListPerformCopy(aux,adj[x]);
+                x++;
+            }
             // PASeriesPerformCopy(ADJ,adj);
             // PAList.PAListPerformCopy(ADJ,adj);
             // PASeries series;
