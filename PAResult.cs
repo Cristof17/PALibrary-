2ab88eb7616cb59fc37190eaef9a1c2b6c72d301
Cristofor Rotsching <cristoforrotsching@Cristofors-MacBook-Pro.local>
2026-06-16 11:38:23 +0300

    Added PAResult.cs

---
+++ PAResult.cs
@@ -0,0 +1,11 @@
+using System.Runtime.InteropServices;
+using PA_Library;
+
+namespace PA_Library
+{
+	public enum PAResult
+	{
+		PARESULT_SUCCESS,
+		PARESULT_FAIL
+	}
+}
--- obj/Debug/netstandard2.0/PA-Library.AssemblyInfo.cs
+++ obj/Debug/netstandard2.0/PA-Library.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PA-Library")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2b5bfe2097fb0c2076b3544d904a89bb5acf989")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2194aeadd25e0a2af374ec6a0711debb2273dda2")]
 [assembly: System.Reflection.AssemblyProductAttribute("PA-Library")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PA-Library")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
