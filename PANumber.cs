01a3819f9095b10b2ac94cd60f85500818fa5063
Cristofor Rotsching <cristoforrotsching@Cristofors-MacBook-Pro.local>
2026-06-16 10:49:57 +0300

    namespace PALibrary in .cs files

--- PANumber.cs
+++ PANumber.cs
@@ -1,19 +1,22 @@
 using System.Runtime.InteropServices;
 using PA_Library;
 
-[StructLayout(LayoutKind.Sequential)]
-protected struct PANumber
+namespace PA_Library
 {
-    int value;
+    [StructLayout(LayoutKind.Sequential)]
+    protected struct PANumber
+    {
+        int value;
 
-    [DllImport("pa")]
-    static extern PANumber PANumberPerformConstruct();
-    [DllImport("pa")]
-    static extern PANumber PANumberPerformInit([MarshalAs(UnmanagedType.IUnknown)]PANumber Number, int Value);
-    [DllImport("pa")]
-    static extern PANumber PANumberPerformCopy([MarshalAs(UnmanagedType.IUnknown)]PANumber from, [MarshalAs(UnmanagedType.IUnknown)]PANumber to);
-    [DllImport("pa")]
-    static extern PANumber PANumberPerformDelete([MarshalAs(UnmanagedType.IUnknown)]PANumber PA);
-    [DllImport("pa")]
-    static extern PANumber PANumberPerformRuin([MarshalAs(UnmanagedType.IUnknown)]PANumber PA);
+        [DllImport("pa")]
+        static extern PANumber PANumberPerformConstruct();
+        [DllImport("pa")]
+        static extern PANumber PANumberPerformInit([MarshalAs(UnmanagedType.IUnknown)] PANumber Number, int Value);
+        [DllImport("pa")]
+        static extern PANumber PANumberPerformCopy([MarshalAs(UnmanagedType.IUnknown)] PANumber from, [MarshalAs(UnmanagedType.IUnknown)] PANumber to);
+        [DllImport("pa")]
+        static extern PANumber PANumberPerformDelete([MarshalAs(UnmanagedType.IUnknown)] PANumber PA);
+        [DllImport("pa")]
+        static extern PANumber PANumberPerformRuin([MarshalAs(UnmanagedType.IUnknown)] PANumber PA);
+    }
 }
