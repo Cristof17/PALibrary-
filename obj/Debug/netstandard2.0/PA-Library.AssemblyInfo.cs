8e424e96e1b6c4f645bdab2f924bca65271a314e
Cristofor Rotsching <cristoforrotsching@Cristofors-MacBook-Pro.local>
2026-06-16 11:14:07 +0300

    eliminat IntrareSiste.cs

--- obj/Debug/netstandard2.0/PA-Library.AssemblyInfo.cs
+++ obj/Debug/netstandard2.0/PA-Library.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PA-Library")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31ab147668ad2529f539523095496deb59f86e2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+389e8d657cab67cef0c0da3b900f4dac38daff98")]
 [assembly: System.Reflection.AssemblyProductAttribute("PA-Library")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PA-Library")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
