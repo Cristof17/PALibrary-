e8b8cf5347917312e9fcd6c64739d42e44d2d42a
Cristofor Rotsching <cristoforrotsching@Cristofors-MacBook-Pro.local>
2026-06-17 11:22:51 +0300

    ADdednfunction free copy fill delete

--- obj/Debug/netstandard2.0/PA-Library.AssemblyInfo.cs
+++ obj/Debug/netstandard2.0/PA-Library.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PA-Library")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ab88eb7616cb59fc37190eaef9a1c2b6c72d301")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fd9acbfbf6df38aa1b6bbade2b488d4280211f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("PA-Library")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PA-Library")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
