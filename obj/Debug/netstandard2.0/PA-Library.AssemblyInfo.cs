23100e106aa10566ca84308f65fdfaac322992f4
Cristofor Rotsching <cristoforrotsching@Cristofors-MacBook-Pro.local>
2026-06-17 12:05:15 +0300

    Untitled codediagram

--- obj/Debug/netstandard2.0/PA-Library.AssemblyInfo.cs
+++ obj/Debug/netstandard2.0/PA-Library.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PA-Library")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fd9acbfbf6df38aa1b6bbade2b488d4280211f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce07ed12e692d2019a782bfe6f5c8b429acb8d14")]
 [assembly: System.Reflection.AssemblyProductAttribute("PA-Library")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PA-Library")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
