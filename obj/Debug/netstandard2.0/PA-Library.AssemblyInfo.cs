4b48835fb97e1abcb5756c18f14a97f22af26340
Cristofor Rotsching <cristoforrotsching@Cristofors-MacBook-Pro.local>
2026-06-17 14:32:33 +0300

    updated file tree, removed StateConstruct.cs

--- obj/Debug/netstandard2.0/PA-Library.AssemblyInfo.cs
+++ obj/Debug/netstandard2.0/PA-Library.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PA-Library")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8430fa5eae9fe528a93ab2afe39aa21b55b52d4f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c50d17f874db877c89a1cac1609dd4c4c65a3a9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("PA-Library")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PA-Library")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
