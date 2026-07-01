b67510ebce72ef9c006bb661d763cacac1f564a8
Cristofor Rotsching <cristoforrotsching@Cristofors-MacBook-Pro.local>
2026-07-01 10:52:08 +0300

    PATree

--- PATree.cs
+++ PATree.cs
@@ -6,16 +6,16 @@ namespace PA
 	[StructLayout(LayoutKind.Sequential)]
 	public struct PATree
 	{
-		public PATree(PACount N, PACount M, PAList ADJ, PAElement Sursa) : this()
+		public PATree(PACount N, PACount M, PAList Adj, PAElement Sursa) : this()
 		{
 			PATree tree;
 			tree = PATreePerformConstruct();
-			tree = PATreePerformInit(tree,N,M,ADJ,Sursa);
+			tree = PATreePerformInit(tree,N,M,Adj,Sursa);
 			// n = PACount.PACountPerformConstruct();
 			n = PACount.PACountPerformCopy(tree.n, n);
 			// m = PACount.PACountPerformConstruct();
 			m = PACount.PACountPerformCopy(tree.m, m);
-			tree.adj = PAList.PAListPerformCopy(ADJ,tree.adj);
+			tree.adj = PAList.PAListPerformCopy(Adj,tree.adj);
 			adj = PAList.PAListPerformCopy(tree.adj,adj);
 			// sursa = PAElement.PAElementPerformConstruct();
 			sursa = PAElement.PAElementPerformCopy(tree.sursa, sursa);
