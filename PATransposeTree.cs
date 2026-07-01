ccc3ad49c3cbeedf2fe468df4b6f762f3c1e9768
Cristofor Rotsching <cristoforrotsching@Cristofors-MacBook-Pro.local>
2026-07-01 14:06:24 +0300

    PATransposeTree signature verified

--- PATransposeTree.cs
+++ PATransposeTree.cs
@@ -24,7 +24,7 @@ namespace PA
 		[DllImport("pa")]
 		internal static extern PATransposeTree PATransposeTreePerformInit([MarshalAs(UnmanagedType.IUnknown)] PATransposeTree Tree, [MarshalAs(UnmanagedType.IUnknown)] PATree Value);
 		[DllImport("pa")]
-		internal static extern PATransposeTree PATransposeTreePerformCopy([MarshalAs(UnmanagedType.IUnknown)] PATransposeTree from, [MarshalAs(UnmanagedType.IUnknown)] PATree to);
+		internal static extern PATransposeTree PATransposeTreePerformCopy([MarshalAs(UnmanagedType.IUnknown)] PATransposeTree from, [MarshalAs(UnmanagedType.IUnknown)] PATransposeTree to);
 		[DllImport("pa")]
 		internal static extern PATransposeTree PATransposeTreePerformDelete([MarshalAs(UnmanagedType.IUnknown)] PATransposeTree PA);
 		[DllImport("pa")]
