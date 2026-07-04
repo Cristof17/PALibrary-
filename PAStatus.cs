fab007286d06779a4bef8428f264fd31b67265b5
Cristofor Rotsching <cristoforrotsching@Cristofors-MacBook-Pro.local>
2026-07-04 22:09:01 +0300

    updated PAStatus

--- PAStatus.cs
+++ PAStatus.cs
@@ -3,35 +3,36 @@ using PA;
 
 namespace PA
 {
-    internal enum PAStatus {
-        STATUS_VISITED=0,
+    // internal enum PAStatus {
+        // STATUS_VISITED=0,
         // s
-        STATUS_NOT_VISITED=1
-    // [StructLayout(LayoutKind.Sequential)]
-    // public struct PAStatus
-    // {
-    //     public PAStatus(int Visited) : this()
-    //     {
-    //         // Visited = visited;
-    //         visited = Visited;
-    //         PAStatus status;
-    //         status = PAStatusPerformConstruct();
-    //         // status = PAStatusPerformInit(status,)
-    //     }
-    //     private int visited;
+        // STATUS_NOT_VISITED=1
+    [StructLayout(LayoutKind.Sequential)]
+    public struct PAStatus
+    {
+        public PAStatus(bool Visited) : this()
+        {
+            // Visited = visited;
+            visited = Visited;
+            // PAStatus status;
+            status = PAStatusPerformConstruct();
+            status = PAStatusPerformInit(status,Visited);
+            // status = PAStatusPerformInit(status,)
+        }
+        private int visited;
 
-    //     public PAResource resource;
+        // public PAResource resource;
 
-    //     [DllImport("pa")]
-    //     internal static extern PAStatus PAStatusPerformConstruct();
-    //     [DllImport("pa")]
-    //     internal static extern PAStatus PAStatusPerformInit([MarshalAs(UnmanagedType.IUnknown)] PAStatus Status, [MarshalAs(UnmanagedType.IUnknown)] PAResource Resource);
-    //     [DllImport("pa")]
-    //     internal static extern PAStatus PAStatusPerformCopy([MarshalAs(UnmanagedType.IUnknown)] PAStatus from, [MarshalAs(UnmanagedType.IUnknown)] PAStatus to);
-    //     [DllImport("pa")]
-    //     internal static extern PAStatus PAStatusPerformDelete([MarshalAs(UnmanagedType.IUnknown)] PAStatus PA);
-    //     [DllImport("pa")]
-    //     internal static extern PAStatus PAStatusPerformRuin([MarshalAs(UnmanagedType.IUnknown)] PAStatus PA);
-    // }
+        [DllImport("pa")]
+        internal static extern PAStatus PAStatusPerformConstruct();
+        [DllImport("pa")]
+        internal static extern PAStatus PAStatusPerformInit([MarshalAs(UnmanagedType.IUnknown)] PAStatus Status, [MarshalAs(UnmanagedType.IUnknown)] int Visited);
+        [DllImport("pa")]
+        internal static extern PAStatus PAStatusPerformCopy([MarshalAs(UnmanagedType.IUnknown)] PAStatus from, [MarshalAs(UnmanagedType.IUnknown)] PAStatus to);
+        [DllImport("pa")]
+        internal static extern PAStatus PAStatusPerformDelete([MarshalAs(UnmanagedType.IUnknown)] PAStatus PA);
+        [DllImport("pa")]
+        internal static extern PAStatus PAStatusPerformRuin([MarshalAs(UnmanagedType.IUnknown)] PAStatus PA);
     }
+    // }
 }
