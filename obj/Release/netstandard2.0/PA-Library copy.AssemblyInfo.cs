825aa4ee4007e7f918d16d9718a27f8989867871
Cric cric <cristof.rotsching@outlook.com>
2026-07-07 13:00:48 +0300

    befoer merge

--- obj/Release/netstandard2.0/PA-Library copy.AssemblyInfo.cs
+++ obj/Release/netstandard2.0/PA-Library copy.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("PA-Library copy")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0.0+0b08d85765f469fe869162c2343612e389ec265e")]
 [assembly: System.Reflection.AssemblyProductAttribute("PA-Library copy")]
