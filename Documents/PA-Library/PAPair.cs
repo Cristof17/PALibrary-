30c841c1e3ad94d231ccb7fb48c4a50db6ed2705
Cristofor Rotsching <cristoforrotsching@Cristofors-MacBook-Pro.local>
2026-07-25 18:50:10 +0300

    PAPair

---
+++ Documents/PA-Library/PAPair.cs
@@ -0,0 +1,36 @@
+//public class PAPereche
+//struct PAPereche
+
+using PA_Library;
+// using System;
+using System.Runtime.InteropServices;
+namespace PA_Library
+{
+    [StructLayout(LayoutKind.Sequential)]
+    protected struct PAPair
+    { 
+        PAElement Node;
+
+        PAElement Neigh;
+
+        [DllImport("pa")]
+        static extern PAPair PAPairConstruct();
+        [DllImport("pa")]
+        static extern PAPair PAPairInit([MarshalAs(UnmanagedType.IUnknown)] PAPair p, [MarshalAs(UnmanagedType.IUnknown)] PAElement Node, [MarshalAs(UnmanagedType.IUnknown)] PAElement Neigh);
+        [DllImport("pa")]
+        static extern int PAPairDelete([MarshalAs(UnmanagedType.IUnknown)] PAElement PA);
+        [DllImport("pa")]
+        static extern int PAPairRuin([MarshalAs(UnmanagedType.IUnknown)] PAPair PA);
+
+        // [StructLayout(LayoutKind.Sequential)]
+        // private struct PAPair Pair;
+
+        // public PAPair(Element Node, Element Neigh)
+        PAPair(PAElement Node, PAElement Neigh) : this()
+        {
+
+        }
+        // PANod node;
+        // PANod neigh;
+    }
+}
