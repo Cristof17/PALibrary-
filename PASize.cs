1fb847d7cb58ca0ded41b81d4210a99e42335cf9
Cristof17 <cristof_rotsching@yahoo.com>
2026-08-20 12:28:23 +0300

    PASize

--- PASize.cs
+++ PASize.cs
@@ -4,6 +4,31 @@ namespace PA
 {
     public readonly partial struct PASize
     {
-        
+        internal readonly unsafe byte* _digits;
+        internal readonly unsafe byte* _value;
+        internal readonly unsafe ulong size;
+
+        public unsafe byte* Digits
+        {
+            get
+            {
+                return _digits;
+            }
+        }
+        public unsafe byte* Value
+        {
+            get
+            {
+                return _value;
+            }
+        }
+
+        public ulong Size
+        {
+            get
+            {
+                return size;
+            }
+        }
     }
 }
